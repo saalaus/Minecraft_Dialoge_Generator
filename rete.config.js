33978f2b6b5d7a118ea6a52b19db01c6ec81a278
sergey <saalaus2@gmail.com>
2022-11-25 20:22:34 +0500

    fix css post

--- rete.config.js
+++ rete.config.js
@@ -1,20 +1,20 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
-import { sass } from 'svelte-preprocess-sass';
+import preprocess from 'svelte-preprocess';
 
 
 export default {
     input: 'src/index.js',
-    name: 'ReteSvelteRenderPluginPlugin',
+    name: 'ReteSvelteRenderPlugin',
     plugins: [
         svelte({
-            preprocess:{
-                style: sass()
-            }
+            preprocess: preprocess(),
+	    emitCss: false
         }),
+
         resolve({
             browser: true,
-            dedupe: ["svelte"]
+            dedupe: ['svelte']
         })
-    ]
+     ]
 }
