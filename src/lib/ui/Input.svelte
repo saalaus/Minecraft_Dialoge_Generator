22604680d89aab757ae010962a77e2b1a0404448
saalaus <saalaus2@gmail.com>
2023-05-05 20:39:32 +0500

    fix styles

--- src/lib/ui/Input.svelte
+++ src/lib/ui/Input.svelte
@@ -23,8 +23,7 @@
     on:input={onInput}
     on:focus
     on:blur
-    bind:this={element}
-/>
+    bind:this={element} />
 
 <style>
     .text-input {
@@ -48,104 +47,104 @@
         white-space: nowrap;
     }
 
-    .text-input :global(.obfuscated) {
+    :global(.obfuscated) {
         text-shadow: 6px 2px 0px;
         padding: 0 8px 0 4px;
     }
 
-    .text-input :global(.bold) {
+    :global(.bold) {
         font-weight: bold;
     }
 
-    .text-input :global(.italic) {
+    :global(.italic) {
         font-style: italic;
     }
 
-    .text-input :global(.strike) {
+    :global(.strike) {
         text-decoration: line-through;
     }
 
-    .text-input :global(.underline) {
+    :global(.underline) {
         text-decoration: underline;
     }
 
-    .text-input :global(.strike.underline) {
+    :global(.strike.underline) {
         text-decoration: underline line-through;
     }
 
-    .text-input :global(.dark_red) {
+    :global(.dark_red) {
         color: rgb(170, 0, 0);
     }
 
-    .text-input :global(.white) {
+    :global(.white) {
         color: white;
     }
 
-    .text-input :global(.black) {
+    :global(.black) {
         color: black;
     }
 
-    .text-input :global(.dark_blue) {
+    :global(.dark_blue) {
         color: rgb(0, 0, 170);
     }
 
-    .text-input :global(.dark_green) {
+    :global(.dark_green) {
         color: rgb(0, 170, 0);
     }
 
-    .text-input :global(.dark_aqua) {
+    :global(.dark_aqua) {
         color: rgb(0, 170, 170);
     }
 
-    .text-input :global(.dark_purple) {
+    :global(.dark_purple) {
         color: rgb(170, 0, 170);
     }
 
-    .text-input :global(.gold) {
+    :global(.gold) {
         color: rgb(255, 170, 0);
     }
 
-    .text-input :global(.gray) {
+    :global(.gray) {
         color: rgb(170, 170, 170);
     }
 
-    .text-input :global(.dark_gray) {
+    :global(.dark_gray) {
         color: rgb(85, 85, 85);
     }
 
-    .text-input :global(.blue) {
+    :global(.blue) {
         color: rgb(85, 85, 255);
     }
 
-    .text-input :global(.green) {
+    :global(.green) {
         color: rgb(85, 255, 85);
     }
 
-    .text-input :global(.aqua) {
+    :global(.aqua) {
         color: rgb(85, 255, 255);
     }
 
-    .text-input :global(.red) {
+    :global(.red) {
         color: rgb(255, 85, 85);
     }
 
-    .text-input :global(.light_purple) {
+    :global(.light_purple) {
         color: rgb(255, 85, 255);
     }
 
-    .text-input :global(.yellow) {
+    :global(.yellow) {
         color: rgb(255, 255, 85);
     }
 
-    .text-input :global(b) {
+    :global(b) {
         font-weight: normal;
     }
 
-    .text-input :global(i) {
+    :global(i) {
         font-style: normal;
     }
 
-    .text-input :global(u) {
+    :global(u) {
         text-decoration: none;
     }
 </style>
